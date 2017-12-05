177231f25d9ecc1c19415d0378cf720813baf2d2
daraeman <daraeman@gmail.com>
2017-12-05 14:53:22 -0800

    version 1.0

--- craigslist_better_styles.user.js
+++ craigslist_better_styles.user.js
@@ -1,58 +1,29 @@
 // ==UserScript==
-// @name        Craigslist Better Styles
-// @namespace   craigslist_better_styles
-// @description Improve Craigslist stylesheets
-// @include     https://*.craigslist.org/search/*
-// @version     1.1
-// @grant       none
+// @name         Craigslist Better Styles
+// @namespace    craigslist_better_styles
+// @description  Moves the 'hide' button to standard location for faster hiding of items
+// @homepageURL  https://github.com/daraeman/craigslist_better_styles
+// @author       daraeman
+// @version      1.0
+// @date         2017-12-05
+// @include      https://*.craigslist.org/search/*
+// @downloadURL  https://github.com/daraeman/craigslist_better_styles/raw/master/craigslist_better_styles.user.js
+// @updateURL    https://github.com/daraeman/craigslist_better_styles/raw/master/craigslist_better_styles.meta.js
 // ==/UserScript==
 
 const script_id = "craigslist_better_styles";
-let last_type;
-const styles = {
-	"thumb": "."+ script_id +" .banish { float: right; }",
-	"gallery": "."+ script_id +" .banish { position: absolute; bottom: 0; right: 0 }",
-};
+const styles = "."+ script_id +" .banish { float: left; margin-right: 5px; margin-top: 3px; }";
 
-function getListType() {
-	let type_chooser_el = document.getElementsByClassName( "search-view" )[0];
-	let selected_el = type_chooser_el.querySelector( ".dropdown-item.sel" );
-	return selected_el.getElementsByTagName( "button" )[0].textContent.trim();
-}
-
-function removeStyles() {
-	let style_el = document.querySelector( "style."+ script_id );
-	if ( style_el )
-		style_el.remove();
-}
-
-function createStyles( type ) {
-
-	if ( ! styles[ type ] )
-		return;
-
+function createStyles() {
 	let style_el = document.createElement( "style" );
-	style_el.textContent = styles[ type ];
+	style_el.textContent = styles;
 	style_el.className = script_id;
 	document.head.appendChild( style_el );
 }
 
-function updateStyles() {
-	let current_type = getListType();
-
-	if ( current_type == last_type )
-		return;
-
-	last_type = current_type;
-
-	removeStyles();
-	createStyles( current_type );
-}
-
 function init() {
 	document.body.className = document.body.className + " " + script_id;
+	createStyles();
 }
 
-
 init();
-updateStyles();
