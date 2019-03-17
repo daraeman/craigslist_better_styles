2bd1eb08eee1f0555dd866f000e09bd7c5452285
David Rae <daraeman@gmail.com>
2019-03-17 13:42:29 -0700

    make code more extendable

--- craigslist_better_styles.user.js
+++ craigslist_better_styles.user.js
@@ -4,17 +4,19 @@
 // @description  Moves the 'hide' button to standard location for faster hiding of items
 // @homepageURL  https://github.com/daraeman/craigslist_better_styles
 // @author       daraeman
-// @version      1.1
-// @date         2017-01-08
+// @version      1.2
+// @date         2019-03-17
 // @include      https://*.craigslist.org/*
 // @downloadURL  https://github.com/daraeman/craigslist_better_styles/raw/master/craigslist_better_styles.user.js
 // @updateURL    https://github.com/daraeman/craigslist_better_styles/raw/master/craigslist_better_styles.meta.js
 // ==/UserScript==
 
 const script_id = "craigslist_better_styles";
-const styles = "."+ script_id +" .banish { float: left; margin-right: 5px; margin-top: 3px; }";
+const styles = {
+	search: "."+ script_id +" .banish { float: left; margin-right: 5px; margin-top: 3px; }",
+};
 
-function createStyles() {
+function createStyles( styles ) {
 	let style_el = document.createElement( "style" );
 	style_el.textContent = styles;
 	style_el.className = script_id;
@@ -25,11 +27,15 @@ function getPage() {
 	return ( /search/.test( document.body.className ) ) ? "search" : false;
 }
 
+function doSearchPage() {
+	document.body.className = document.body.className + " " + script_id;
+	createStyles( styles.search );
+}
+
 function init() {
 	let page = getPage();
 	if ( page === "search" ) {
-		document.body.className = document.body.className + " " + script_id;
-		createStyles();
+		doSearchPage();
 	}
 }
 
